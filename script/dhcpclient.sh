9b7c8c2b404a1590cbf40cba0c9ec3d45c7bd213
Daniel Mauser <dmauser@hotmail.com>
2023-06-05 11:28:08 -0500

    update

--- script/dhcpclient.sh
+++ script/dhcpclient.sh
@@ -1,4 +1,23 @@
 #!/bin/sh
+# This script has been tested on Ubuntu
+# Update repository
+apt update -y && sudo apt upgrade -y 
+#Traceroute
+apt-get install traceroute -y
+# TCP traceroute
+apt-get install tcptraceroute -y
+# Nmap
+apt-get install nmap -y
+# Hping3
+apt-get install hping3 -y
+# iPerf
+apt-get install iperf3 -y
+# Nginx and adds machine name on main page
+apt-get install nginx -y && hostname > /var/www/html/index.html
+# Speedtest
+apt-get install speedtest-cli -y
+# net-tools
+apt-get install net-tools -y
+
 ## Install the DHCP Client Tools 
-apt-get update
 apt-get install -y kea-common kea-admin
